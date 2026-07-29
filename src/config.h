5646cac7ec84e5276735f9455751b5dedbf730a3
vvb333007 <vvb333007@gmail.com>
2026-07-29 22:59:11 +0700

    +	description added

--- src/config.h
+++ src/config.h
@@ -13,12 +13,19 @@
 
 #pragma once
 
-//#define CONFIG_TARFS_LOG        1       /*!< Enable verbose logging, lots of text! for development or bug hunting */
+/**
+ * Edit this file to change TARFS compile-time setting
+ * 
+ *
+ */
+
+#define CONFIG_TARFS_VERSION 0x00000104 /*!< Version number: 0.1.4 */
+//#define CONFIG_TARFS_LOG       1      /*!< Enable verbose logging, lots of text! for development or bug hunting */
 
 #define CONFIG_TARFS_MAX_FS  4          /*!< Max number of mounted TARFS filesystems */
 #define CONFIG_TARFS_MAX_FDS 16         /*!< Max number of active opened files (per filesystem, must be < 33) */
 
-//#define CONFIG_TARFS_INTEGRITY  1       /*!< Check data integrity records. FS must be processed with tarsum utility or mount will fail */
+//#define CONFIG_TARFS_INTEGRITY  1     /*!< Check data integrity records. FS must be processed with tarsum utility or mount will fail */
 #define CONFIG_TARFS_EXTMEM 1           /*!< Use external memory where available (e.g. PSRAM on ESP32) */
 
 #define CONFIG_TARFS_HAVE_FDOPENDIR  1  /*!< Support for fdopendir() */
@@ -28,11 +35,11 @@
 #define CONFIG_TARFS_HAVE_SENDFILE 1    /*!< Support for zero-overhead sendfile() */
 
 
-#define CONFIG_TARFS_COUNTERS 1        /*!< Support runtime stats */
-//#define CONFIG_TARFS_BIG_ENDIAN 1      /*!< Set to 1 on bige-endian architectures */
+#define CONFIG_TARFS_COUNTERS 1         /*!< Support runtime stats */
+//#define CONFIG_TARFS_BIG_ENDIAN 1     /*!< Set to 1 on bige-endian architectures */
 //#define CONFIG_TARFS_HAVE_STATVFS_H 1 /*!< If platform provides its own sys/statvfs.h, this macro shuould be uncommented */
 
-/* tarsum utility settings */
+/* tarsum utility settings, please do not change */
 #ifdef TARSUM_BUILD
 #  undef CONFIG_TARFS_LOG
 #  undef CONFIG_TARFS_HAVE_FDOPENDIR  /* Incompatible with glibc targets (e.g. Linux or Cygwin)*/
